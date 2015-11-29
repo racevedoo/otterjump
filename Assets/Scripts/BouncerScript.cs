31b2fb77127830ded9ec779fc89b4e597e0a94cd
Rafael Acevedo <raa7@cin.ufpe.br>
2015-11-28 21:03:13 -0300

    Initial prototype version - fix platform generation

---
+++ Assets/Scripts/BouncerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BouncerScript : MonoBehaviour {
+
+    public float bounceFactor = 4f;
+
+    private float minX;
+    private float minY;
+    private float maxY;
+    private float maxX;
+
+
+    // Use this for initialization
+    void Start () {
+        float vert = Camera.main.orthographicSize;
+        float hor = vert * Screen.width / Screen.height;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            // toggle visibility:
+            Renderer renderer = GetComponent<Renderer>();
+            Collider2D collider = GetComponent<Collider2D>();
+            renderer.enabled = !renderer.enabled;
+            collider.enabled = !collider.enabled;
+            transform.position = transform.position - new Vector3(transform.position.x + 0.1f, 0f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        other.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceFactor, ForceMode2D.Impulse);
+    }
+
+}
