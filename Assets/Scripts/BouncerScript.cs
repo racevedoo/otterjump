505500d12b5e19c589e31b2e5e4a3fd214db28b1
Rafael Acevedo <raa7@cin.ufpe.br>
2015-12-07 13:25:05 -0300

    Adding platform spawner, scores and boundaries

--- Assets/Scripts/BouncerScript.cs
+++ Assets/Scripts/BouncerScript.cs
@@ -4,21 +4,31 @@ using System.Collections;
 public class BouncerScript : MonoBehaviour {
 
     public float bounceFactor = 4f;
+    public float repositionDelay = 0.1f;
 
-    private float minX;
-    private float minY;
-    private float maxY;
-    private float maxX;
+    private float minX = 0;
+    private float minY = 0;
+    private float maxY = 10;
+    private float maxX = 8.9f;
 
+    private int _Score;
+    private bool countScore = true;
+    private GUIText _ScoreGUI;
+    private Collider2D _Collider;
+    private Vector2 _OriginPosition;
 
     // Use this for initialization
-    void Start () {
-        float vert = Camera.main.orthographicSize;
-        float hor = vert * Screen.width / Screen.height;
+    void Start ()
+    {
+        _OriginPosition = transform.position;
+        GameObject.Find("HighScoreValue").GetComponent<GUIText>().text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+        _ScoreGUI =  GameObject.Find("ScoreValue").GetComponent<GUIText>();
+        _Collider = GetComponent<Collider2D>();
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         if (Input.GetKeyDown(KeyCode.Z))
         {
             // toggle visibility:
@@ -32,8 +42,31 @@ public class BouncerScript : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        other.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        other.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceFactor, ForceMode2D.Impulse);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (countScore)
+            {
+                _Score++;
+                _ScoreGUI.text = _Score.ToString();
+                countScore = false;
+            }
+            other.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            other.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceFactor, ForceMode2D.Impulse);            
+            Invoke("SpawnPlatform", repositionDelay);
+        }        
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        countScore = true;
+    }
+
+    void SpawnPlatform()
+    {
+        transform.position = new Vector2(Random.Range(minX, maxX), transform.position.y);
+
+        if (_Score > PlayerPrefs.GetInt("HighScore"))
+            PlayerPrefs.SetInt("HighScore", System.Convert.ToInt32(_ScoreGUI.text));
     }
 
 }
---
+++ Assets/Scripts/MovementBounder.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementBounder : MonoBehaviour {
+
+    public float maxX = 10f;
+    public float minX = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x <= minX)
+        {
+            transform.position = new Vector2(minX, transform.position.y);
+        }
+        else if (transform.position.x >= maxX)
+        {
+            transform.position = new Vector2(maxX, transform.position.y);
+        }
+    }
+}
